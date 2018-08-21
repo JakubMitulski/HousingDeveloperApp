ef54e4a83cd074c8b88d0b3d7f3782b896f69512
JakubMitulski <jakub.mitulski@gmail.com>
2018-08-21 12:03:16 +0200

    Initial commit

---
+++ src/main/java/capgemini/HousingDeveloperApp.java
@@ -0,0 +1,13 @@
+package capgemini;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HousingDeveloperApp {
+
+	public static void main(String[] args) {
+		System.setProperty("spring.profiles.active", "mysql");
+		SpringApplication.run(HousingDeveloperApp.class, args);
+	}
+}
